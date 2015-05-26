96ce8283817f09c6973f89d2530cb2327aa7a32f
Puneet Singh Ludu <pludu@factset.com>
2015-05-26 16:08:21 -0400

    Some Changes

--- src/example/MainClass.java
+++ src/example/MainClass.java
@@ -11,8 +11,8 @@ public class MainClass {
 	public static void main(String [ ] args){
 		PSHashMap<Boolean> shm1 = new PSHashMap<Boolean>(new SoundexBigram());
 		PSHashMap<Boolean> shm2 = new PSHashMap<Boolean>(new SameString());
-		shm1.put("My name is puneet", true);
-		System.out.println(shm1.containsKey("mai naam iz punit"));
+		shm1.put("thik is Hojpethol", true);
+		System.out.println(shm1.containsKey("This is hospitol"));
 		shm2.put("My name is puneet", true);
 		System.out.println(shm2.containsKey("mai naam iz punit"));
 	}
--- src/info/puneetsingh/fuzzyhashmap/algos/FuzzyStrings.java
+++ src/info/puneetsingh/fuzzyhashmap/algos/FuzzyStrings.java
@@ -3,7 +3,7 @@ package info.puneetsingh.fuzzyhashmap.algos;
 public abstract class FuzzyStrings {
 	/**   
      * Method to get string to find in dictionary
-     * //@author puneet
+     * @author puneet
 	 *
      * @param text as <code>String</code> to find the value in dictionary
      * @return  Fuzzy value as <code>String</code>
